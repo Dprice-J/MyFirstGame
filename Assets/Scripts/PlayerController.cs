c318ab509569e145ae8e96dd6393babddabb72e8
Dprice-J <67209799+Dprice-J@users.noreply.github.com>
2020-06-27 16:03:26 -0700

    Add mouse look

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    public float lookSpeed = 1f;
+    public Transform camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -17,5 +19,12 @@ public class PlayerController : MonoBehaviour
     {
         transform.Translate(Vector3.forward * moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
         transform.Translate(Vector3.right * moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+        transform.Rotate(Vector3.up * lookSpeed * Input.GetAxis("Mouse X") * Time.deltaTime);
+        Quaternion initialRotation = camera.rotation;
+        camera.Rotate(Vector3.left * lookSpeed * Input.GetAxis("Mouse Y") * Time.deltaTime);
+        if (camera.rotation.x < Mathf.Deg2Rad * -40 || camera.rotation.x > Mathf.Deg2Rad * 40) {
+            camera.rotation = initialRotation;
+        }
+        Debug.Log(camera.rotation.x * Mathf.Rad2Deg);
     }
 }
