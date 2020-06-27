60308b3a051c3dc64161e9a59695392343f6d950
Dprice-J <67209799+Dprice-J@users.noreply.github.com>
2020-06-27 14:29:00 -0700

    Initial commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float initialForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(transform.rotation * Vector3.up * initialForce, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space")) {
+            GameObject newBullet = Instantiate(
+                bulletPrefab,
+                transform.position + transform.forward * bulletPrefab.transform.position.z,
+                transform.rotation * bulletPrefab.transform.rotation
+            );
+        }
+    }
+}
